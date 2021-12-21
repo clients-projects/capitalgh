639ae6ecc44f916b45d9326189020199680f7b60
Igboanugwo Collins <munisco12@gmail.com>
2021-12-21 08:28:00 +0100

    working on table

--- client/src/views/siteOwner/WithdrawalRequests.jsx
+++ client/src/views/siteOwner/WithdrawalRequests.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react'
 import { Grid, Row, Col, Table } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css'
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit'
 
 import * as actions from '../../store/actions/burgerIndex'
 
@@ -51,9 +57,70 @@ const PendingWithdrawals = (props) => {
         }
     }
 
-    // if (props.investNowApprovalSuccess) {
-    //     console.log('the approval', props.investNowApprovalSuccess)
-    // }
+     const allWithdrawals = []
+
+     if (allUsersWithdrawal.length > 0) {
+         console.log({ allUsersWithdrawal })
+         allUsersWithdrawal.map((value) => {
+             const { fundNO, creator, amount, currency, updatedAt } = value
+             allWithdrawals.push({
+                 id: fundNO,
+                 username: creator,
+                 amount,
+                 currency,
+                 date: updatedAt,
+             })
+         })
+     }
+
+     const columns = [
+         { dataField: 'id', text: 'Id', sort: true },
+         { dataField: 'username', text: 'Username', sort: true },
+         { dataField: 'amount', text: 'Amount Withdrawn', sort: true },
+         { dataField: 'currency', text: 'Currency', sort: true },
+         { dataField: 'date', text: 'Date', sort: true },
+     ]
+
+     const defaultSorted = [
+         {
+             dataField: 'name',
+             order: 'desc',
+         },
+     ]
+
+     const pagination = paginationFactory({
+         page: 1,
+         sizePerPage: 5,
+         lastPageText: '>>',
+         firstPageText: '<<',
+         nextPageText: '>',
+         prePageText: '<',
+         showTotal: true,
+         alwaysShowAllBtns: true,
+         onPageChange: function (page, sizePerPage) {
+             console.log('page', page)
+             console.log('sizePerPage', sizePerPage)
+         },
+         onSizePerPageChange: function (page, sizePerPage) {
+             console.log('page', page)
+             console.log('sizePerPage', sizePerPage)
+         },
+     })
+
+     const { SearchBar, ClearSearchButton } = Search
+
+     const MyExportCSV = (props) => {
+         const handleClick = () => {
+             props.onExport()
+         }
+         return (
+             <div>
+                 <button className='btn btn-success' onClick={handleClick}>
+                     Export to CSV
+                 </button>
+             </div>
+         )
+     }
     return (
         <div className='content'>
             <Grid fluid>
