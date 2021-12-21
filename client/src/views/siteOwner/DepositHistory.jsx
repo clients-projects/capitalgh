cd88f3f8676480433930ae2bd737b757e035ed60
Igboanugwo Collins <munisco12@gmail.com>
2021-12-21 07:38:59 +0100

    working on table

--- client/src/views/siteOwner/DepositHistory.jsx
+++ client/src/views/siteOwner/DepositHistory.jsx
@@ -38,21 +38,12 @@ const AllUsersDepositHistory = (props) => {
     console.log(thWithdrawalArray)
 
     const products = [
-        { id: 1, name: 'George', animal: 'Monkey' },
-        { id: 2, name: 'Jeffrey', animal: 'Giraffe' },
-        { id: 3, name: 'Alice', animal: 'Giraffe' },
-        { id: 4, name: 'Foster', animal: 'Tiger' },
-        { id: 5, name: 'Tracy', animal: 'Bear' },
-        { id: 6, name: 'Joesph', animal: 'Lion' },
-        { id: 7, name: 'Tania', animal: 'Deer' },
-        { id: 8, name: 'Chelsea', animal: 'Tiger' },
-        { id: 9, name: 'Benedict', animal: 'Tiger' },
-        { id: 10, name: 'Chadd', animal: 'Lion' },
-        { id: 11, name: 'Delphine', animal: 'Deer' },
-        { id: 12, name: 'Elinore', animal: 'Bear' },
-        { id: 13, name: 'Stokes', animal: 'Tiger' },
-        { id: 14, name: 'Tamara', animal: 'Lion' },
-        { id: 15, name: 'Zackery', animal: 'Bear' },
+        { id: 1, username: 'George', amount: '1000' },
+        { id: 2, username: 'Jeffrey', amount: '4844' },
+        { id: 3, name: 'Alice', amount: '2093' },
+        { id: 4, name: 'Alice', amount: '2093' },
+        { id: 5, name: 'Alice', amount: '2093' },
+  
     ]
     const columns = [
         { dataField: 'id', text: 'Id', sort: true },
