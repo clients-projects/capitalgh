94236c6ff86a509e5301aa60f32accac2e267904
Igboanugwo Collins <munisco12@gmail.com>
2021-12-31 00:56:36 +0100

    working on user detail profit update

--- src/views/siteOwner/UserDetails.jsx
+++ src/views/siteOwner/UserDetails.jsx
@@ -61,8 +61,7 @@ const UserDetails = (props) => {
                 setUserDeposits(props.memberDeposits)
             }
         }
-            console.log({ profitData })
-
+        console.log({ profitData })
     }, [props, parsed.id, profitData])
 
     const handleChange = (e) => {
@@ -121,20 +120,19 @@ const UserDetails = (props) => {
 
     const handleMember = (e, fundNO) => {
         e.persist()
-        console.log({fundNO})
-        const value =  Number(e.target.value)
+        console.log({ fundNO })
+        const value = Number(e.target.value)
         // setProfitData((oldArr) => [...oldArr,{ oldArr: e.target.value }])
-        
 
-       console.log({profitData})
-       let newProfitValueArr = profitData
+        console.log({ profitData })
+        let newProfitValueArr = profitData
 
-       console.log('arr profit update', profitData[fundNO - 1][fundNO])
-       newProfitValueArr[fundNO - 1][fundNO] = value
+        console.log('arr profit update', profitData[fundNO - 1][fundNO])
+        newProfitValueArr[fundNO - 1][fundNO] = value
 
-       console.log({newProfitValueArr})
+        console.log({ newProfitValueArr })
 
-       setProfitData(newProfitValueArr)
+        setProfitData(newProfitValueArr)
     }
 
     const updateMemberProfit = (id) => {
@@ -144,7 +142,6 @@ const UserDetails = (props) => {
 
         for (let i = 0; i < props.memberId.length; i++) {
             if (id === i) {
-              
                 props.onInitUpdateProfit(
                     profitData[i][i + 1],
                     props.memberId[i]._id,
@@ -153,7 +150,6 @@ const UserDetails = (props) => {
             }
         }
     }
-
 
     useEffect(() => {
         if (props.member) {
@@ -204,7 +200,7 @@ const UserDetails = (props) => {
             })
         }
 
-        console.log({userDeposits})
+        console.log({ userDeposits })
     }, [userDeposits])
 
     const handleSubmit = (e) => {
@@ -242,50 +238,52 @@ const UserDetails = (props) => {
 
     const usersDepositData = []
 
-    if (userDeposits.length > 0) {
-        userDeposits.map((value, index) => {
-            const { fundNO, amount, planName, updatedAt } = value
+    useEffect(() => {
+        if (userDeposits.length > 0) {
+            userDeposits.map((value, index) => {
+                const { fundNO, amount, planName, updatedAt } = value
 
-            let keepProfitIndex = {}
+                let keepProfitIndex = {}
 
-            console.log({profitData})
+                console.log({ profitData })
 
-            if (profitData[index]) {
-                profitRef.current = profitData[index]
-                keepProfitIndex = profitRef.current
-            }
+                if (profitData[index]) {
+                    profitRef.current = profitData[index]
+                    keepProfitIndex = profitRef.current
+                }
 
-            console.log('keepProfit outer', keepProfitIndex[fundNO])
+                console.log('keepProfit outer', keepProfitIndex[fundNO])
 
-            usersDepositData.push({
-                id: fundNO,
-                amount,
-                plan: planName,
-                profit: (
-                    <>
-                        <input
-                            type='number'
-                            value={keepProfitIndex[fundNO]}
-                            onChange={(e) => handleMember(e, fundNO)}
-                            name={fundNO}
-                            className='member__profit'
-                        />
-                    </>
-                ),
-                date: updatedAt,
-                action: (
-                    <>
-                        <button
-                            className='btn1'
-                            onClick={() => updateMemberProfit(fundNO - 1)}
-                        >
-                            {props.loading ? 'Loading...' : 'Update Profit'}
-                        </button>
-                    </>
-                ),
+                usersDepositData.push({
+                    id: fundNO,
+                    amount,
+                    plan: planName,
+                    profit: (
+                        <>
+                            <input
+                                type='number'
+                                value={keepProfitIndex[fundNO]}
+                                onChange={(e) => handleMember(e, fundNO)}
+                                name={fundNO}
+                                className='member__profit'
+                            />
+                        </>
+                    ),
+                    date: updatedAt,
+                    action: (
+                        <>
+                            <button
+                                className='btn1'
+                                onClick={() => updateMemberProfit(fundNO - 1)}
+                            >
+                                {props.loading ? 'Loading...' : 'Update Profit'}
+                            </button>
+                        </>
+                    ),
+                })
             })
-        })
-    }
+        }
+    }, [handleMember, profitData, props.loading, updateMemberProfit, userDeposits, usersDepositData])
 
     const columns = [
         { dataField: 'id', text: 'Id', sort: true },
