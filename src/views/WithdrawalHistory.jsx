bd72499e9fdcaa659050c69f33ee13660fe08266
Igboanugwo Collins <munisco12@gmail.com>
2022-01-07 08:41:36 +0100

    added pagination table

--- src/views/WithdrawalHistory.jsx
+++ src/views/WithdrawalHistory.jsx
@@ -1,6 +1,12 @@
 import React, { useState, useEffect, useRef } from 'react'
-import { Grid, Row, Col, Table } from 'react-bootstrap'
+import { Grid, Row, Col } from 'react-bootstrap'
 import { connect } from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import 'react-bootstrap-table-next/dist/react-bootstrap-table2.css'
+import 'react-bootstrap-table2-paginator/dist/react-bootstrap-table2-paginator.min.css'
+import BootstrapTable from 'react-bootstrap-table-next'
+import paginationFactory from 'react-bootstrap-table2-paginator'
+import ToolkitProvider, { Search } from 'react-bootstrap-table2-toolkit'
 
 import * as orderAction from '../store/actions/burgerIndex'
 
@@ -31,6 +37,74 @@ const Members = (props) => {
         }
     }, [props])
 
+    const depositHistory = []
+
+    if (getDepositHistory.length > 0) {
+        getDepositHistory.map((value) => {
+            const { fundNO, creator, amount, planName, updatedAt } = value
+            console.log({ value })
+
+            const creatorEmail = creator.email
+
+            return depositHistory.push({
+                id: fundNO,
+                email: creatorEmail,
+                amount,
+                plan: planName,
+                date: updatedAt,
+            })
+        })
+    }
+
+    const columns = [
+        { dataField: 'id', text: 'Id', sort: true },
+        { dataField: 'package', text: 'package', sort: true },
+        { dataField: 'amount', text: 'amount', sort: true },
+        { dataField: 'profit', text: 'profit', sort: true },
+        { dataField: 'date', text: 'Date', sort: true },
+    ]
+
+    const defaultSorted = [
+        {
+            dataField: 'name',
+            order: 'desc',
+        },
+    ]
+
+    const pagination = paginationFactory({
+        page: 1,
+        sizePerPage: 5,
+        lastPageText: '>>',
+        firstPageText: '<<',
+        nextPageText: '>',
+        prePageText: '<',
+        showTotal: true,
+        alwaysShowAllBtns: true,
+        onPageChange: function (page, sizePerPage) {
+            console.log('page', page)
+            console.log('sizePerPage', sizePerPage)
+        },
+        onSizePerPageChange: function (page, sizePerPage) {
+            console.log('page', page)
+            console.log('sizePerPage', sizePerPage)
+        },
+    })
+
+    const { SearchBar, ClearSearchButton } = Search
+
+    const MyExportCSV = (props) => {
+        const handleClick = () => {
+            props.onExport()
+        }
+        return (
+            <div>
+                <button className='btn btn-success' onClick={handleClick}>
+                    Export to CSV
+                </button>
+            </div>
+        )
+    }
+
     return (
         <div className='content'>
             <Grid fluid>
@@ -43,40 +117,31 @@ const Members = (props) => {
                             ctTableFullWidth
                             ctTableResponsive
                             content={
-                                <Table>
-                                    <thead>
-                                        <tr>
-                                            {thInvestmentHistoryArray.map(
-                                                (prop, key) => {
-                                                    return (
-                                                        <th key={key}>
-                                                            {prop}
-                                                        </th>
-                                                    )
-                                                }
-                                            )}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        {getWithdrawalHistory.map(
-                                            (prop, key) => {
-                                                return (
-                                                    <tr key={key}>
-                                                        {Object.values(
-                                                            prop
-                                                        ).map((prop, key) => {
-                                                            return (
-                                                                <td key={key}>
-                                                                    {prop}
-                                                                </td>
-                                                            )
-                                                        })}
-                                                    </tr>
-                                                )
-                                            }
-                                        )}
-                                    </tbody>
-                                </Table>
+                                <ToolkitProvider
+                                    bootstrap4
+                                    data={depositHistory}
+                                    keyField='id'
+                                    columns={columns}
+                                    search
+                                    exportCSV
+                                >
+                                    {(props) => (
+                                        <div>
+                                            <SearchBar {...props.searchProps} />{' '}
+                                            <ClearSearchButton
+                                                {...props.searchProps}
+                                            />
+                                            <hr />
+                                            <MyExportCSV {...props.csvProps} />
+                                            <BootstrapTable
+                                                defaultSorted={defaultSorted}
+                                                classes='table-layout-auto custom-table'
+                                                pagination={pagination}
+                                                {...props.baseProps}
+                                            />
+                                        </div>
+                                    )}
+                                </ToolkitProvider>
                             }
                         />
                     </Col>
